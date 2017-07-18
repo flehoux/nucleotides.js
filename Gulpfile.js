6f1cf64ea289dcfdcaddd7aa0dbed02ab15ac7f0
matehat <mathieu@damours.org>
2017-07-18 11:16:32 -0400

    Add karma runner

---
+++ Gulpfile.js
@@ -0,0 +1,39 @@
+var browserify = require('browserify')
+var gulp = require('gulp')
+var source = require('vinyl-source-stream')
+var buffer = require('vinyl-buffer')
+var globby = require('globby')
+var through = require('through2')
+var TestServer = require('karma').Server
+
+gulp.task('test', ['javascript'], function (done) {
+  new TestServer({
+    configFile: require('path').join(__dirname, 'karma.conf.js'),
+    singleRun: !(process.argv.includes('watch'))
+  }, done).start()
+})
+
+gulp.task('javascript', function () {
+  var bundledStream = through()
+
+  bundledStream
+    .pipe(source('test.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('./dist/'))
+
+  globby(['./spec/**/*_spec.js']).then(function (entries) {
+    browserify({entries: entries})
+    .transform('babelify', {presets: ['es2015']})
+    .bundle()
+    .pipe(bundledStream)
+  }).catch(function (err) {
+    bundledStream.emit('error', err)
+  })
+
+  return bundledStream
+})
+
+gulp.task('watch', function () {
+  gulp.watch('spec/**/*.js', ['javascript'])
+  gulp.watch('src/**/*.js', ['javascript'])
+})
