57d73bef95d300e0200e79693f22def152f303ba
matehat <mathieu@damours.org>
2017-10-07 12:50:18 -0400

    Expose EmittingArray from nucleotides
    # Conflicts:
    #	index.js

--- index.js
+++ index.js
@@ -2,13 +2,38 @@ const Model = require('./src/model')
 const Mixin = require('./src/mixin')
 const Protocol = require('./src/protocol')
 const Collection = require('./src/collection')
+const EmittingArray = require('./src/emitting_array')
+
+let promiseFactory = function (...args) {
+  return new Promise(...args)
+}
+
+promiseFactory.resolve = Promise.resolve.bind(Promise)
+promiseFactory.all = Promise.all.bind(Promise)
 
 module.exports = {
   Model,
   Mixin,
   Protocol,
   Collection,
+  EmittingArray,
   makeEmitter: require('./src/emitter')
+
+  setPromiseFactory: function (factory) {
+    promiseFactory = factory
+  },
+  resolvePromise: function (arg) {
+    return promiseFactory.resolve(arg)
+  },
+  rejectPromise: function (arg) {
+    return promiseFactory.reject(arg)
+  },
+  allPromise: function (arg) {
+    return promiseFactory.all(arg)
+  },
+  createPromise: function (...args) {
+    return promiseFactory(...args)
+  }
 }
 
 let protocols = require('./src/protocols')
