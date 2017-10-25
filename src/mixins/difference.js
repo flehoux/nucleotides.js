1582084a77be61c4f82fdb930ee5e37446dee997
Mathieu D'Amours <mathieu@damours.org>
2017-10-25 18:35:51 -0400

    Add forking mechanic in DifferenceMixin

--- src/mixins/difference.js
+++ src/mixins/difference.js
@@ -1,5 +1,6 @@
 const {Mixin} = require('../..')
 const $$difference = Symbol('difference')
+const $$discardFn = Symbol('discard')
 
 const DifferenceMixin = Mixin('DifferenceMixin')
   .construct(function (options = {}) {
@@ -8,6 +9,47 @@ const DifferenceMixin = Mixin('DifferenceMixin')
   })
   .method('$setPristine', function () {
     this[$$difference] = {}
+  })
+  .method('$fork', function () {
+    let forked = this.$clone(this.$isNew)
+    let original = this
+    Object.defineProperty(forked, '$original', { get () { return original } })
+    Object.defineProperty(forked, '$forked', { get () { return true } })
+    let cb = function (diff) {
+      let changes = {}
+      for (let key in diff) {
+        if (!forked.$difference.hasOwnProperty(key)) {
+          changes[key] = this.$clean[key]
+        }
+      }
+      forked.$updateAttributes(changes, {broadcasted: true})
+    }
+    this.$on('change', cb)
+    forked[$$discardFn] = () => {
+      original = null
+      this.$off('change', cb)
+      delete this[$$discardFn]
+    }
+    return forked
+  })
+  .method('$discard', function () {
+    if (typeof this[$$discardFn] === 'function') {
+      this[$$discardFn]()
+    }
+  })
+  .method('$rollback', function () {
+    this.$updateAttributes(this.$original.$clean, {broadcasted: true})
+    this.$setPristine()
+  })
+  .method('$commitChanges', function () {
+    if (this.$original != null || !this.$isPristine) {
+      let changes = {}
+      for (let key in this.$difference) {
+        changes[key] = this.$clean[key]
+      }
+      this.$original.$updateAttributes(changes)
+      this.$setPristine()
+    }
   })
   .derive('$isPristine', function () {
     return Object.keys(this[$$difference]).length === 0
