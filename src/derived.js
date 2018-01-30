e6bfb2e4b1ca0252f456b79ac91f01b7645d37f2
matehat <mathieu@damours.org>
2018-01-30 16:57:29 -0500

    Pass previous value of cached derived to getter

--- src/derived.js
+++ src/derived.js
@@ -73,8 +73,8 @@ class CachedDerivedValue extends DerivedValue {
     }
   }
 
-  cache (object) {
-    object[this.$$cache] = this.getter.call(object)
+  cache (object, changeset) {
+    object[this.$$cache] = this.getter.call(object, object[this.$$cache], changeset)
   }
 
   clearCache (object) {
@@ -94,13 +94,11 @@ class CachedDerivedValue extends DerivedValue {
     }
 
     Object.defineProperty(klass.prototype, derived.name, {
-      get: function () {
+      get () {
         if (this.hasOwnProperty(derived.$$cache)) {
           return this[derived.$$cache]
         } else {
-          if (derived.options.eager === false) {
-            derived.cache(this)
-          }
+          derived.cache(this)
           return this[derived.$$cache]
         }
       }
@@ -111,18 +109,18 @@ class CachedDerivedValue extends DerivedValue {
     if (Array.isArray(this.options.source) && this.options.source.length > 0) {
       for (let attributeName of this.options.source) {
         if (changeset.$keys.has(attributeName)) {
-          this.update(object)
+          this.update(object, changeset)
           return
         }
       }
     } else if (this.options.source == null || this.options.source === 'all') {
-      this.update(object)
+      this.update(object, changeset)
     }
   }
 
-  update (object) {
-    if (this.options.eager === true) {
-      this.cache(object)
+  update (object, changeset) {
+    if (this.options.eager === true || object.hasOwnProperty(this.$$cache)) {
+      this.cache(object, changeset)
     } else {
       this.clearCache(object)
     }
@@ -135,12 +133,9 @@ class AsyncDerivedValue extends CachedDerivedValue {
     this.$$promise = Symbol('promise')
   }
 
-  clearCache (object, reensure = true) {
-    let loaded = object.hasOwnProperty(this.$$cache)
+  clearCache (object) {
+    delete object[this.$$cache]
     delete object[this.$$promise]
-    if (loaded && reensure) {
-      this.ensure(object)
-    }
   }
 
   force (object, value) {
@@ -157,41 +152,38 @@ class AsyncDerivedValue extends CachedDerivedValue {
     }
 
     Object.defineProperty(klass.prototype, derived.name, {
-      get: function () {
+      get () {
         return this[derived.$$cache]
       }
     })
   }
 
-  update (object) {
-    this.clearCache(object)
+  update (object, changeset) {
+    if (this.fetched(object) && this.options.reensure !== false) {
+      this.ensure(object, changeset)
+    } else {
+      this.clearCache(object)
+    }
   }
 
   fetched (object) {
     return object.hasOwnProperty(this.$$cache)
   }
 
-  ensure (object) {
+  ensure (object, changeset) {
     let derived = this
     if (object.hasOwnProperty(derived.$$promise)) {
       return object[derived.$$promise]
     }
-    let result = this.getter.call(object)
-    if (result == null) {
-      return null
-    } else if (typeof result.then === 'function') {
-      object[derived.$$promise] = result.then(function (value) {
-        object[derived.$$cache] = value
-        object.$emit('resolved', derived.name, value)
-        object.constructor.$emit('resolved', object, derived.name, value)
-        return object
-      })
-      return object[derived.$$promise]
-    } else {
-      const {resolvePromise} = require('..')
-      object[derived.$$promise] = resolvePromise(result)
-      return object[derived.$$promise]
-    }
+    let result = this.getter.call(object, object[this.$$cache], changeset)
+    const {resolvePromise} = require('..')
+    object[derived.$$promise] = resolvePromise(result).then(function (value) {
+      object[derived.$$cache] = value
+      object.$emit('resolved', derived.name, value)
+      object.constructor.$emit('resolved', object, derived.name, value)
+      return object
+    })
+    return object[derived.$$promise]
   }
 }
 
